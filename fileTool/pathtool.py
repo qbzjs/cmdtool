31e47ee4a179ccaf0c31ef59b26c655cda0f565f
woodcol <fengmm521@gmail.com>
2017-05-17 12:00:25 +0800

    路径工具

---
+++ fileTool/pathtool.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+#png8图片批量转png24
+import os
+import sys
+#获取脚本路径
+def cur_file_dir():
+    pathx = sys.argv[0]
+    tmppath,_file = os.path.split(pathx)
+    if cmp(tmppath,'') == 0:
+        tmppath = sys.path[0]
+    #判断为脚本文件还是py2exe编译后的文件，如果是脚本文件，则返回的是脚本的目录，如果是py2exe编译后的文件，则返回的是编译后的文件路径
+    if os.path.isdir(tmppath):
+        return tmppath
+    elif os.path.isfile(tmppath):
+        return os.path.dirname(tmppath)
+    
+#获取父目录
+def GetParentPath(strPath):
+    if not strPath:
+        return None;
+    lsPath = os.path.split(strPath);
+    if lsPath[1]:
+        return lsPath[0];
+    lsPath = os.path.split(lsPath[0]);
+    return lsPath[0];
+
+#获取目录下的所有类型文件
+def getAllExtFile(pth,fromatx = ".erl"):
+    jsondir = pth
+    jsonfilelist = []
+    for root, _dirs, files in os.walk(jsondir):
+        for filex in files:          
+            #print filex
+            name,text = os.path.splitext(filex)
+            if cmp(text,fromatx) == 0:
+                jsonArr = []
+                rootdir = pth
+                dirx = root[len(rootdir):]
+                pathName = dirx +os.sep + filex
+                jsonArr.append(pathName)
+                (newPath,_name) = os.path.split(pathName)
+                jsonArr.append(newPath)
+                jsonArr.append(name)
+                jsonfilelist.append(jsonArr)
+            elif fromatx == ".*" :
+                jsonArr = []
+                rootdir = pth
+                dirx = root[len(rootdir):]
+                pathName = dirx +os.sep + filex
+                jsonArr.append(pathName)
+                (newPath,_name) = os.path.split(pathName)
+                jsonArr.append(newPath)
+                jsonArr.append(name)
+                jsonfilelist.append(jsonArr)
+    return jsonfilelist
+
+#获取一个路径中所包含的所有目录及子目录
+def getAllLevelDirs(dirpths):
+    dirleves = []
+    dirtmp = ''
+    for d in dirpths:
+        dirtmp += '/' + d
+        dirleves.append(dirtmp)
+    return dirleves
+
+#在outpth目录下创建ndir路径中的所有目录，是否使用决对路径
+def makeDir(outpth,ndir):
+    tmpdir = ''
+    if ndir[0] == '/':
+        tmpdir = outpth + ndir
+    else:
+        tmpdir = outpth + '/' + ndir
+    print tmpdir
+    if not os.path.exists(tmpdir):
+        os.mkdir(tmpdir)
+
+# 创建一个目录下的所有子目录到另一个目录
+def createDirs(spth,tpth):
+    files = getAllExtFile(spth,'.*')
+    makedirstmp = []
+    isOK = True
+    # 分析所有要创建的目录
+    tmpfpth = fpth
+    for d in files:
+        if d[1] != '/' and (not d[1] in makedirstmp): #创建未创建的目录层级
+            tmpdir = d[1][1:]
+            tmpleves = tmpdir.split('/')
+            alldirs = getAllLevelDirs(tmpleves)
+            for dtmp in alldirs:
+                if not dtmp in makedirstmp:
+                    makeDir(tpth,dtmp)
+                    makedirstmp.append(dtmp)
